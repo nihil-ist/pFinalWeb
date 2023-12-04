bd792e0819091800ac4234e21d8b070fd77ab177
“AngelReyes” <angel.ivan.konige@gmail.com>
2023-12-04 09:14:46 -0600

    ultimos detalles sobre altas bajas y cambios

--- ABC/altas.php
+++ ABC/altas.php
@@ -1,34 +1,33 @@
 <?php
-$nombre_servidor = "localhost:33066";
-$nombre_usuario = "root";
-$contraseña = "";
-$nombre_base_datos = "havenrecords";
-
-$conn = new mysqli($nombre_servidor, $nombre_usuario, $contraseña, $nombre_base_datos);
-
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-
-$sql = "SHOW TABLES";
-$resultado = $conn->query($sql);
-
-$tablas = array();
-if ($resultado->num_rows > 0) {
-    while ($fila = $resultado->fetch_row()) {
-        if (strpos($fila[0], 'rel_') !== 0) {
-            $tablas[] = $fila[0];
+    $nomImg = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["imagen"]) && !(empty($_FILES["imagen"]["tmp_name"]))) {
+        $targetDir = "images/";  // Directorio donde se guardarán las imágenes
+        $targetFile = $targetDir . basename($_FILES["imagen"]["name"]);
+        $nomImg = htmlspecialchars(basename($_FILES["imagen"]["name"]));
+        $check = getimagesize($_FILES["imagen"]["tmp_name"]);
+        if ($check !== false) {
+            if (move_uploaded_file($_FILES["imagen"]["tmp_name"], $targetFile)) {
+            }
         }
     }
-}
+    $nombre_servidor = "localhost:33066";
+    $nombre_usuario = "root";
+    $contraseña = "";
+    $nombre_base_datos = "havenrecords";
 
-$insertR = false;
-$errorR = false;
-$sql_columnas = "SHOW COLUMNS FROM productos";
-$resultado_columnas = $conn->query($sql_columnas);
+    $conn = new mysqli($nombre_servidor, $nombre_usuario, $contraseña, $nombre_base_datos);
 
- // Preparar los nombres de las columnas para la inserción
-$column_names = [];
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+    }
+
+    $insertR = false;
+    $errorR = false;
+    $sql_columnas = "SHOW COLUMNS FROM productos";
+    $resultado_columnas = $conn->query($sql_columnas);
+
+    // Preparar los nombres de las columnas para la inserción
+    $column_names = [];
     while ($fila = $resultado_columnas->fetch_assoc()) {
         $column_names[] = $fila['Field'];
     }
@@ -37,22 +36,26 @@ $column_names = [];
     if (isset($_POST['submit'])) {
         $values = [];
         foreach ($column_names as $column) {
+            echo $column ."\t";
             if (isset($_POST[$column])) {
                 $values[] = "'" . $conn->real_escape_string($_POST[$column]) . "'";
-            } else {
+            }
+            elseif (isset($_FILES[$column])) {
+                $values[] = "'" . $nomImg . "'";
+            }
+             else {
                 $values[] = "NULL";
             }
         }
-
+    
         $sql_insert = "INSERT INTO productos (" . implode(', ', $column_names) . ") VALUES (" . implode(', ', $values) . ")";
         if ($conn->query($sql_insert) === TRUE) {
             $insertR = true;
         } else {
             $errorR = true;
+            echo "Error al insertar en la base de datos: " . $conn->error;
         }
     }
-
-
 $conn->close();
 ?>
 
@@ -94,14 +97,52 @@ $conn->close();
     <h1 class="title mb-5">Altas</h1>
 <h2 class="text-center">Selecciona una tabla</h2>
         <div class="mt-4">
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" enctype="multipart/form-data">
             <table>
-            <?php foreach ($column_names as $column): ?>
                 <tr>
-                <td><label class="form-label" for="<?php echo $column; ?>"><?php echo $column; ?>:</label></td>
-                <td><input id="<?php echo $column; ?>" class="form-control bg-dark text-white" type="text" name="<?php echo $column; ?>"><br></td>
+                    <td><label class="form-label" for="idProducto">idProducto</label></td>
+                    <td><input id="idProducto" class="form-control bg-dark text-white" type="number" name="idProducto"><br></td>
                 </tr>
-            <?php endforeach; ?>
+                <tr>
+                    <td><label class="form-label" for="nombreProducto">nombreProducto</label></td>
+                    <td><input id="nombreProducto" class="form-control bg-dark text-white" type="text" name="nombreProducto"><br></td>
+                </tr>
+                <tr>
+                    <td><label class="form-label" for="descripcion">descripcion</label></td>
+                    <td><input id="descripcion" class="form-control bg-dark text-white" type="text" name="descripcion"><br></td>
+                </tr>
+                <tr>
+                    <td><label class="form-label" for="existencias">existencias</label></td>
+                    <td><input id="existencias" class="form-control bg-dark text-white" type="number" name="existencias"><br></td>
+                </tr>
+                <tr>
+                    <td><label class="form-label" for="precio">precio</label></td>
+                    <td><input id="precio" class="form-control bg-dark text-white" type="number" name="precio"><br></td>
+                </tr>
+                <tr>
+                    <td><label class="form-label" for="imagen">imagen</label></td>
+                    <td><input id="imagen" class="form-control bg-dark text-white" type="file" name="imagen"><br></td>
+                </tr>
+                <tr>
+                    <td><label class="form-label" for="descuento">descuento</label></td>
+                    <td><input id="descuento" class="form-control bg-dark text-white" type="number" name="descuento"><br></td>
+                </tr>
+                <tr>
+                    <td><label class="form-label" for="categoria">categoria</label></td>
+                    <td><input id="categoria" class="form-control bg-dark text-white" type="text" name="categoria"><br></td>
+                </tr>
+                <tr>
+                    <td><label class="form-label" for="noCanciones">noCanciones</label></td>
+                    <td><input id="noCanciones" class="form-control bg-dark text-white" type="number" name="noCanciones"><br></td>
+                </tr>
+                <tr>
+                    <td><label class="form-label" for="artista">artista</label></td>
+                    <td><input id="artista" class="form-control bg-dark text-white" type="text" name="artista"><br></td>
+                </tr>
+                <tr>
+                    <td><label class="form-label" for="ano">ano</label></td>
+                    <td><input id="ano" class="form-control bg-dark text-white" type="number" name="ano"><br></td>
+                </tr>
             </table>
             <div class="text-center">
                 <input class="mt-3 btn btn-success" type="submit" name="submit" value="Insertar Registro">
