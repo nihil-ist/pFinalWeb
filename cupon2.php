169df7a249cb5ed6599bb6dcfc6f8d9058ab37f7
AaronRufo <ar440481@gmail.com>
2023-12-11 01:08:26 -0600

    cupon 2

---
+++ cupon2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>POST</title>
+    <link rel="stylesheet" href="">
+   
+
+</head>
+<body>
+
+<div style="border: 5px dotted #bbb; width: 80%;border-radius: 15px; margin: 0 auto; max-width: 600px;background-color: #f1f1f1;
+    padding: 20px;">
+  <div class="jumbotron">
+    <h3>Haven Records</h3>
+  </div>
+  <div class="jumbotron" style="background-color:white">
+    <h2><b>GET 20% DISCOUNT ON YOUR PURCHASE</b></h2>
+    <p>Find the best music albums and take advantage of this exclusive offer.</p>
+  </div>
+  <div class="jumbotron">
+    <p>Use the promotional code: <span style="background: #ccc; padding: 3px;">HAVE20</span></p>
+    <p class="expire">Expires: 31/12/2023</p>
+    <button style=" background-color: #6a5acd;border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px;
+    cursor: pointer;" onclick="usarCupon()">Get cupon!!</button>
+  </div>
+</div>
+<script>
+function usarCupon() {
+  /*Cambiar alert*/ 
+  alert('Cupon aplied: GET 20% DISCOUNT ON YOUR PURCHASE!!');
+}
+</script>
+
+ 
+
+
